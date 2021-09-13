e55a631959033ab9e6da4b8a02270814f1acae4b
Shevin Mamandi <shevin1987@gmail.com>
2021-09-13 12:15:55 +0200

    Initial commit

---
+++ src/main/java/no/kristiania/http.java
@@ -0,0 +1,29 @@
+package no.kristiania;
+
+import javax.imageio.IIOException;
+import java.io.IOException;
+import java.net.Socket;
+
+public class http {
+
+    public http(String host, int port, String requestTarget) {
+
+    }
+
+    public int getStatusCode() {
+        return 0;
+    }
+
+    public static void main(String[]args) throws IOException {
+        Socket socket= new Socket( "httpbin.org",80);
+
+            socket.getOutputStream().write(
+                    ("GET / html HTTP/1.1\r\n" +
+                  "Host: httpbin.org\r\n" +
+                            "\r\n").getBytes()
+            );
+
+
+
+    }
+}
